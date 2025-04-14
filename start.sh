13db6c7cff204e53e7c306045525a69c9abbab00
Frank Chang <frank.chang1000@gmail.com>
2025-04-14 16:39:00 -0400

    more prod stuff

--- start.sh
+++ start.sh
@@ -1,10 +1,17 @@
-#!/usr/bin/env bash
+=#!/usr/bin/env bash
+set -e  # Exit if any command fails
 
-# Run migrations
+# If PORT is not set, default to 8000
+if [ -z "$PORT" ]; then
+  echo "PORT is not set. Defaulting to 8000."
+  PORT=8000
+fi
+
+echo "Running migrations..."
 python manage.py migrate --noinput
 
-# Collect static
+echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
-# Start Gunicorn
+echo "Starting Gunicorn on port $PORT..."
 gunicorn poketrade.wsgi:application --bind 0.0.0.0:$PORT
