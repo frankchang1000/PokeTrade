2184fe182fbc1fd3d597f5d50d4984834b6131a3
Frank Chang <frank.chang1000@gmail.com>
2025-04-21 20:31:50 -0400

    fix edge case where user not authenticated tries to view collection

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 # Create your views here.
@@ -21,3 +22,7 @@ def register(request):
     else:
         form = UserRegisterForm()
     return render(request, 'accounts/register.html', {'form': form})
+
+@login_required
+def accounts(request):
+    return render(request, 'accounts/accounts.html')
